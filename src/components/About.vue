fe66bdc6967ab49eabd93a4634752e139d55b62f
Maxorik <gms441@mail.ru>
2019-10-31 18:04:32 +0700

    add About part, finish main styles

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,21 @@
 <template>
     <section>
         <div class="about-here">
-            <p>About here</p>
+            <div class='busket-sum about-text'>
+                <p style="text-indent:2em;">Наша пекарня принципиально не использует искусственные добавки и усилители вкуса при изготовлении продукции.</p>
+                <p style="text-indent:2em;">Для своего производства мы выбираем только свежие и натуральные ингредиенты.</p>
+                <p style="text-indent:2em;">Все наши изделия выпекаются в духовке, а не жарятся. Следовательно, в них нет канцерогенов, образующихся при длительном нагреве масла, и приносящих большой вред нашему организму.</p>
+                <p style="text-indent:2em;">Кроме того, благодаря технологии выпечки и соблюдению температурного режима сохраняются витамины и полезные вещества.</p>
+                
+                <div class='what-btn'>
+                    <router-link class='btn-route' to='/products'>Что же у нас есть?</router-link>
+                </div>
+            </div>
+            
+            
+            <div class='about-img-d'>
+                <img src='../img/about.jpg' class='about-img'>
+            </div>
         
         </div>
     </section>
--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,12 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-import about from './about'
 import busket from './busket'
 import order from './order'
 import products from './products'
 
 export default new Vuex.Store({
     modules: {
-        about,
         busket,
         order,
         products
